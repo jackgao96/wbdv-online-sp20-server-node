6dcb1a55b67e6ac0f876d944a43b6802b33a3f5a
jackgao118 <gao.hui1@husky.neu.edu>
2020-04-02 17:54:27 -0400

    node server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+
+require('./controllers/quizzes.controllers.server')(app)
+require('./controllers/questions.controller.server')(app)
+app.get('/', (req, res) => res.send('hello world'))
+
+app.listen(3000)
+
